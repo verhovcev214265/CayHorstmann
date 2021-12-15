4aed8300abe846f3aeb5325045b6dfc18ab6b57b
Mark <verhovcev214265@gmail.com>
2021-12-15 23:00:56 +0200

    commit into branch main

---
+++ src/git/branches/test/Main.java
@@ -0,0 +1,25 @@
+package git.branches.test;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean exit = false;
+        while (!exit){
+
+            System.out.println("Enter your name: \n" +
+                    "If you want to go out enter (wq)");
+            Scanner scan = new Scanner(System.in);
+            String input = scan.nextLine();
+
+            if (input.equals("wq")){
+                System.out.println("Bye - bye");
+                exit = true;
+            }else {
+                Master master = new Master();
+                master.greeting(input);
+            }
+        }
+    }
+
+}
---
+++ src/git/branches/test/Master.java
@@ -0,0 +1,8 @@
+package git.branches.test;
+
+public class Master {
+
+    public void greeting(String input){
+        System.out.println("Hello " + input + " welcome to branch Master.");
+    }
+}
